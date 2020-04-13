b3f3339a1a72ee711207374040227b97dd76537e
joaoalc <up201806531@fe.up.pt>
2020-04-13 22:59:15 +0100

    Commit temporaria?

--- MyCylinder.js
+++ MyCylinder.js
@@ -34,15 +34,10 @@ class MyPyramid extends CGFobject {
             // in each face will be different
 
             var sa=Math.sin(ang);
-            //var saa=Math.sin(ang+alphaAng);
             var ca=Math.cos(ang);
-            //var caa=Math.cos(ang+alphaAng);
 
             this.vertices.push(ca, -0.5, -sa);
             this.vertices.push(ca, 0.5, -sa);
-            //this.vertices.push(caa, -0.5, -saa);
-            //this.vertices.push(caa, 0.5, -saa);
-            
             this.indices.push(2 * i - 1,2 * i - 2, 2 * i + 1);
             this.indices.push(2 * i - 2 , 2 * i, 2 * i + 1);
 
@@ -74,37 +69,11 @@ class MyPyramid extends CGFobject {
 
 
 
-            /*
-            normal = [
-                caa, 0,  -saa
-            ]
-
             
-            // normalization
-            var nsize=Math.sqrt(
-                normal[0]*normal[0]+
-                normal[1]*normal[1]+
-                normal[2]*normal[2]
-                );
-            normal[0]/=nsize;
-            normal[1]/=nsize;
-            normal[2]/=nsize;
-
-            // push normal once for each vertex of this triangle
-            this.normals.push(...normal);
-            this.normals.push(...normal);
-
-            //this.indices.push(6*i, (6*i+1) , (6*i+2) );
-            */
             
 
             ang+=alphaAng;
         }
-
-        //this.vertices.push(1, -0.5, 0);
-        //this.vertices.push(1, 0.5, 0);
-        //this.texCoords.push(1, 1);
-        //this.texCoords.push(1, 0);
 
         ang = 0;
         for(var i = 0; i < this.slices; i++){
