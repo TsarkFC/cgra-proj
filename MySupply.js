886533a80ad4071bc12877023a0a459179148b50
TsarkFC <digasromao2000@gmail.com>
2020-05-16 19:14:55 +0100

    Box textures

--- MySupply.js
+++ MySupply.js
@@ -17,9 +17,7 @@ class MySupply extends CGFobject {
         this.state = SupplyStates.INACTIVE;
         this.initMaterials(this.scene);
 
-        this.squareSide = new MyQuad(this.scene);
-        this.squareTop = new MyQuad(this.scene);
-        this.squareBottom = new MyQuad(this.scene);
+        this.square = new MyQuad(this.scene);
         this.maxy = 15;
         this.y = 15;
         this.x = 0;
@@ -35,26 +33,8 @@ class MySupply extends CGFobject {
         this.texture.setDiffuse(0.9, 0.9, 0.9, 1);
         this.texture.setSpecular(0.1, 0.1, 0.1, 1);
         this.texture.setShininess(10.0);
-        this.texture.loadTexture('images/supply_temp.png');
+        this.texture.loadTexture('images/woodbox.jpg');
         this.texture.setTextureWrap('REPEAT', 'REPEAT');
-
-        //------ Texture Material
-        this.textureTop = new CGFappearance(scene);
-        this.textureTop.setAmbient(0.1, 0.1, 0.1, 1);
-        this.textureTop.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.textureTop.setSpecular(0.1, 0.1, 0.1, 1);
-        this.textureTop.setShininess(10.0);
-        this.textureTop.loadTexture('images/terrain.jpg');
-        this.textureTop.setTextureWrap('REPEAT', 'REPEAT');
-
-        //------ Texture Material
-        this.textureBottom = new CGFappearance(scene);
-        this.textureBottom.setAmbient(0.1, 0.1, 0.1, 1);
-        this.textureBottom.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.textureBottom.setSpecular(0.1, 0.1, 0.1, 1);
-        this.textureBottom.setShininess(10.0);
-        this.textureBottom.loadTexture('images/heightmap.jpg');
-        this.textureBottom.setTextureWrap('REPEAT', 'REPEAT');
     }
     
 	display(){
@@ -71,95 +51,84 @@ class MySupply extends CGFobject {
 
             this.scene.pushMatrix();
             this.scene.translate(0,0,0.5);
-            this.squareSide.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.pushMatrix();
             this.scene.translate(0,0,-0.5);
             this.scene.rotate(Math.PI, 0, 1, 0);
-            this.squareSide.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.pushMatrix();
             this.scene.translate(0.5,0,0);
             this.scene.rotate(Math.PI / 2, 0, 1, 0);
-            this.squareSide.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.pushMatrix();
             this.scene.translate(-0.5,0,0);
             this.scene.rotate(3*Math.PI / 2, 0, 1, 0);
-            this.squareSide.display();
+            this.square.display();
             this.scene.popMatrix();
-
-            this.textureBottom.apply();
-            if (this.scene.linearInt) this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
-            //else this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             
             this.scene.pushMatrix();
             this.scene.translate(0,-0.5,0);
             this.scene.rotate(Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
+            this.square.display();
             this.scene.popMatrix();
-
-            this.textureTop.apply();
-            if (this.scene.linearInt) this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
-            //else this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 
             this.scene.pushMatrix();
             this.scene.translate(0,0.5,0);
             this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareTop.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.popMatrix();
         }
         else if(this.state = SupplyStates.LANDED){
-            this.textureBottom.apply();
-            if (this.scene.linearInt) this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
-            //else this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-            
-            this.scene.pushMatrix();
-            this.scene.translate(0 + this.x,-0.5 + this.y,0 + this.z);
-            this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
-            this.scene.popMatrix();
-
             this.texture.apply();
             if (this.scene.linearInt) this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
-            //else this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+            
+            this.scene.pushMatrix();
+            this.scene.translate(this.x,-0.5 + this.y,0 + this.z);
+            this.scene.rotate(-Math.PI/2, 1, 0, 0);
+            this.square.display();
+            this.scene.popMatrix();
 
             this.scene.pushMatrix();
-            this.scene.translate(1 + this.x, -0.5 + this.y, 0 + this.z);
+            this.scene.translate(this.x, -0.5 + this.y, 1 + this.z);
+            this.scene.rotate(Math.PI/3, 0, 1, 0);
             this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.pushMatrix();
             this.scene.translate(-1 + this.x, -0.5 + this.y, 0 + this.z);
+            this.scene.rotate(Math.PI/5, 0, 1, 0);
             this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.pushMatrix();
-            this.scene.translate(0 + this.x,-0.5 + this.y, 1 + this.z);
+            this.scene.translate(0.5 + this.x,-0.5 + this.y, 1 + this.z);
+            this.scene.rotate(Math.PI/10, 0, 1, 0);
             this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
+            this.square.display();
             this.scene.popMatrix();
 
             this.scene.pushMatrix();
-            this.scene.translate(0 + this.x, -0.5 + this.y, -1 + this.z);
+            this.scene.translate(this.x, -0.5 + this.y, -1 + this.z);
+            this.scene.rotate(Math.PI/7, 0, 1, 0);
             this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
+            this.square.display();
             this.scene.popMatrix();
 
-            this.textureTop.apply();
-            if (this.scene.linearInt) this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
-            //else this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.scene.pushMatrix();
-            this.scene.translate(0 + this.x, -0.5 + this.y, -2 + this.z);
+            this.scene.translate(0.5 + this.x, -0.5 + this.y, -1.5 + this.z);
+            this.scene.rotate(Math.PI/5, 0, 1, 0);
             this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.squareBottom.display();
+            this.square.display();
             this.scene.popMatrix();
 
         }
@@ -197,21 +166,15 @@ class MySupply extends CGFobject {
         this.state = SupplyStates.INACTIVE;
     }
 
-    updateBuffers(complexity){
-        this.squareSide.updateBuffers();
-        this.squareTop.updateBuffers();
-        this.squareBottom.updateBuffers();
+    updateBuffers(){
+        this.square.updateBuffers();
     }
 	
 	enableNormalViz(){
-        this.squareSide.enableNormalViz();
-        this.squareTop.enableNormalViz();
-        this.squareBottom.enableNormalViz();
+        this.square.enableNormalViz();
     }
 	disableNormalViz(){
-        this.squareSide.disableNormalViz();
-        this.squareTop.disableNormalViz();
-        this.squareBottom.disableNormalViz();
+        this.square.disableNormalViz();
     }
 }
 
