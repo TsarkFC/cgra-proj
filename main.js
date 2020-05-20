73633eee713dcd9e31bf045c0a0e74570bad4da3
TsarkFC <digasromao2000@gmail.com>
2020-05-20 09:35:45 +0100

    Small change so that pages work properly

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 include=function(){function f(){var a=this.readyState;(!a||/ded|te/.test(a))&&(c--,!c&&e&&d())}var a=arguments,b=document,c=a.length,d=a[c-1],e=d.call;e&&c--;for(var g,h=0;c>h;h++)g=b.createElement("script"),g.src=arguments[h],g.async=!0,g.onload=g.onerror=g.onreadystatechange=f,(b.head||b.getElementsByTagName("head")[0]).appendChild(g)};
 serialInclude=function(a){var b=console,c=serialInclude.l;if(a.length>0)c.splice(0,0,a);else b.log("Done!");if(c.length>0){if(c[0].length>1){var d=c[0].splice(0,1);b.log("Loading "+d+"...");include(d,function(){serialInclude([]);});}else{var e=c[0][0];c.splice(0,1);e.call();};}else b.log("Finished.");};serialInclude.l=new Array();
 
-serialInclude(['../lib/CGF.js','MyScene.js', 'MyInterface.js', 'objects/MySphere.js', 'objects/MyCylinder.js', 'objects/MyUnitCube.js', 'vehicle/MyVehicle.js', 'vehicle/MyRudder.js', 'objects/MyTriangleBig.js', 'objects/MyQuad.js', 'objects/MyPlane.js', 'MySupply.js', 'vehicle/MyVehicleRudders.js', 'vehicle/MyEngine.js', 'vehicle/MyGondola.js', 'MyFlag.js', 'MyBillboard.js',
+serialInclude(['lib/CGF.js','MyScene.js', 'MyInterface.js', 'objects/MySphere.js', 'objects/MyCylinder.js', 'objects/MyUnitCube.js', 'vehicle/MyVehicle.js', 'vehicle/MyRudder.js', 'objects/MyTriangleBig.js', 'objects/MyQuad.js', 'objects/MyPlane.js', 'MySupply.js', 'vehicle/MyVehicleRudders.js', 'vehicle/MyEngine.js', 'vehicle/MyGondola.js', 'MyFlag.js', 'MyBillboard.js',
 
 main=function()
 {
